90b68eeabdd5cefd8342cb496d1d103c260d2dcc
bebi21 <nguyenlocnd18@gmail.com>
2023-08-21 10:09:35 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,39 @@
+// 1 console.log(" wellcome to  Rekki Academy");
+
+// 2 prompt("moi banj  nhap  so  tuoi  cua minh");
+
+// 3 document.write("quyet tam hoc lap trinh");
+
+// 4 alert("chao mung ban den voi  rekki")
+
+// 5 confirm("ban du 18 tuoi")
+
+// let ly = Number(prompt("ly"));
+
+// let hoa = +prompt("hoa");
+// let sinh = +prompt("sinh");
+
+// let ly;
+// let hoa;
+// let sinh;
+// ly = +prompt("ly");
+// hoa = +prompt("hoa");
+// sinh = +prompt("sinh");
+
+// let tong = ly + hoa + sinh;
+// let tb = tong / 3;
+
+// console.log(`diem tong`, tong);
+// console.log(`diem tb  `, tb);
+// console.log(`diem mon  toan ${tb}`);
+
+// let C =+prompt("nhap do C");
+// let F = (9 * C) / 5 + 32;
+// console.log(F)
+
+// let bk = +prompt(" ban kinh la");
+// //  let result =
+// //  console.log(Math.PI)
+// const PI = 3.14;
+// let dt = bk ** 2 * PI;
+// console.log("dien tich hinh tron", dt); x;
